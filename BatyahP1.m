2308708fdb4d5122a5ce7590f410c967e825b808
SmOkImOkEy <ronmisgav@gmail.com>
2014-09-29 13:13:32 +0300

    added head and chin

--- BatyahP1.m
+++ BatyahP1.m
@@ -2,5 +2,8 @@
 
 phi=linspace(0,2*pi,100);
 plot(cos(phi),sin(phi));
-
+hold on
 disp('this is 1st branch.. work?')
+plot(.25*cos(phi),1.25+.25*sin(phi))
+plot(.25*cos(phi),-1.25-.25*sin(phi))
+axis([-2 2 -2 2])
